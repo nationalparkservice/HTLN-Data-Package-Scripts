263f230fad71cf387c11063cb7b3edd842c18b6e
HeartlandNetwork <gareth_rowell@nps.gov>
2023-12-07 15:14:47 -0600

    reach measures completed

--- fish/src/ReachMeasurementsQC.R
+++ fish/src/ReachMeasurementsQC.R
@@ -94,7 +94,7 @@ t <- reachmeasurements |>
    print(n = 310)
 
 
-reachmeasurements$TimeSampled_hhmm_mil <-  str_sub(reachmeasurements$TimeSampled_hhmm_mil, 
+reachmeasurements$TimeSampled_hhmm_mil <- str_sub(reachmeasurements$TimeSampled_hhmm_mil, 
                                     start = 12, end = -1)
 
  
@@ -114,4 +114,100 @@ c <- reachmeasurements |>
 ggplot(c, aes(x = WaterTemp_C)) +
   geom_histogram(binwidth = 1)
  
+# Variables: Reach measurements - AirTemp_C ------------------------------------
+
+ggplot(reachmeasurements, aes(x = AirTemp_C)) +
+  geom_histogram(binwidth = 1)
+
+c <- reachmeasurements |>
+  filter(AirTemp_C > 0)
+
+ggplot(c, aes(x = AirTemp_C)) +
+  geom_histogram(binwidth = 1)
+
+
+# Variables: Reach measurements - pH -------------------------------------------
+
+ggplot(reachmeasurements, aes(x = pH)) +
+  geom_histogram(binwidth = 1)
+
+c <- reachmeasurements |>
+  filter(pH > 0)
+
+ggplot(c, aes(x = pH)) +
+  geom_histogram(binwidth = .05)
+
+
+# Variables: Reach measurements - SpecificConductance_microScm -----------------
+
+
+ggplot(reachmeasurements, aes(x = SpecificConductance_microScm)) +
+  geom_histogram(binwidth = 1)
+
+c <- reachmeasurements |>
+  filter(SpecificConductance_microScm > 0)
+
+ggplot(c, aes(x = SpecificConductance_microScm)) +
+  geom_histogram(binwidth = 50)
+
+
+# Variables: Reach measurements - Conductivity_microScm ------------------------
+
+
+ggplot(reachmeasurements, aes(x = Conductivity_microScm)) +
+  geom_histogram(binwidth = 1)
+
+c <- reachmeasurements |>
+  filter(Conductivity_microScm > 0)
+
+ggplot(c, aes(x = Conductivity_microScm)) +
+  geom_histogram(binwidth = 10)
+
+# Variables: Reach measurements - DissolvedOxygen_mgL ------------------------
+
+
+ggplot(reachmeasurements, aes(x = DissolvedOxygen_mgL)) +
+  geom_histogram(binwidth = 1)
+
+c <- reachmeasurements |>
+  filter(DissolvedOxygen_mgL > 0)
+
+ggplot(c, aes(x = DissolvedOxygen_mgL)) +
+  geom_histogram(binwidth = .1)
+
+# Variables: Reach measurements - SecchiTube ------------------------
+
+ggplot(reachmeasurements, aes(x = SecchiTube)) +
+  geom_histogram(binwidth = 1)
+
+c <- reachmeasurements |>
+  filter(SecchiTube > 0)
+
+ggplot(c, aes(x = SecchiTube)) +
+  geom_histogram(binwidth = 5)
+
+c <- reachmeasurements |>
+  filter(SecchiTube > 100)
+
+ggplot(c, aes(x = SecchiTube)) +
+  geom_histogram(binwidth = 1)
+
+reachmeasurements |> distinct(SecchiTube)  |>  
+  print(n = 186)
+
+
+# Variable: Units ------------------------
+
+reachmeasurements |> distinct(Units)  |>  
+  print(n = 186)
+
+# keep?
+
+
+
+
+
+
+
+
 
